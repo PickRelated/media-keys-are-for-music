9002163fc38266eb5af973b951ca6df4b0a824ad
Igor Tatarintsev <ietatarin@gmail.com>
2025-07-24 22:51:47 +0200

    Prettier

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 chrome.runtime.onMessage.addListener((command) => {
-  console.info(`Command "${command}" recieved`);
-  const playBarDiv =
-    document.querySelector('div[class^="PlayerBar"') || document;
+  console.info(`Command "${command}" recieved`)
+  const playBarDiv = document.querySelector('div[class^="PlayerBar"') || document
 
   switch (command) {
-    case "play": {
-      (
+    case 'play': {
+      ;(
         playBarDiv.querySelector('button[aria-label="Playback"]') ||
         playBarDiv.querySelector('button[aria-label="Pause"]')
-      )?.click();
-      break;
+      )?.click()
+      break
     }
-    case "next": {
-      playBarDiv.querySelector('button[aria-label="Next song"]')?.click();
-      break;
+    case 'next': {
+      playBarDiv.querySelector('button[aria-label="Next song"]')?.click()
+      break
     }
-    case "previous": {
-      playBarDiv.querySelector('button[aria-label="Previous song"]')?.click();
-      break;
+    case 'previous': {
+      playBarDiv.querySelector('button[aria-label="Previous song"]')?.click()
+      break
     }
   }
-});
+})
